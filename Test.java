74a9d427a1597929d585c90c693b85a004b1fd55
test <sdfsd>
2025-04-12 17:35:12 +0800

    1

---
+++ Test.java
@@ -0,0 +1,12 @@
+package org.check;
+
+@Table("ABCD")
+public class Test {
+
+    @Column("efg")
+    public String name;
+    public String age;
+    public static void main(String[] args){
+        System.out.println("1111");
+    }
+}
